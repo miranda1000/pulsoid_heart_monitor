99a4c347bf8f2f8fc432c0f719e1698e4979f661
Wyko <wyko.ter.haar@gmail.com>
2023-07-11 18:21:33 +0200

    .

--- pagmon/main.py
+++ pagmon/main.py
@@ -1,17 +1,36 @@
 import configparser
 
 import httpx
+import time
 
 config = configparser.ConfigParser()
 config.read("config.ini")
 
 
 class Pulsoid:
-    def validate_token(self):
+    def __init__(self):
+        """Initialize the Pulsoid class."""
+        self.token = config["Login"]["API_TOKEN"]
+        self._get_client()
+        self._validate_token()
+
+    def get_heartrate(self):
+        """Get the current heart rate."""
+        try:
+            r = self.client.get("https://dev.pulsoid.net/api/v1/user")
+            r.raise_for_status()
+        except httpx.HTTPError as e:
+            print(e)
+            return None
+        return r.json()["heartrate"]
+
+    def _validate_token(self):
+        """Validate token against the API."""
         r = self.client.get("https://dev.pulsoid.net/api/v1/token/validate")
         r.raise_for_status()
 
-    def get_client(self):
+    def _get_client(self) -> httpx.Client:
+        """Get a client with the token set in the header."""
         self.client = httpx.Client(
             headers={
                 "Authorization": f"Bearer {self.token}",
@@ -19,11 +38,20 @@ class Pulsoid:
             }
         )
 
-    def __init__(self):
-        self.token = config["Login"]["API_TOKEN"]
-        self.get_client()
-        self.validate_token()
+
+def save_heart_rate(hr):
+    """Save the heart rate to a file, catching exceptions since there might be race conditions."""
+    try:
+        with open(config["Settings"]["HEART_RATE_FILE"], "w") as f:
+            f.write(hr)
+    except Exception as e:
+        print(e)
 
 
 if __name__ == "__main__":
     pulsoid = Pulsoid()
+
+    while True:
+        if hr := pulsoid.get_heartrate():
+            save_heart_rate(hr)
+            print(hr)
