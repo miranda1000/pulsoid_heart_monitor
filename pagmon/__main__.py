351ca5300f6ff61768beff7bceda5affbe1e7712
Wyko <wyko.ter.haar@gmail.com>
2023-07-11 18:43:59 +0200

    Version One Ready

--- pagmon/__main__.py
+++ pagmon/__main__.py
@@ -4,7 +4,6 @@ import httpx
 import time
 
 config = configparser.ConfigParser()
-config.read("config.ini")
 
 
 class Pulsoid:
@@ -17,17 +16,26 @@ class Pulsoid:
     def get_heartrate(self):
         """Get the current heart rate."""
         try:
-            r = self.client.get("https://dev.pulsoid.net/api/v1/user")
+            r = self.client.get("https://dev.pulsoid.net/api/v1/data/heart_rate/latest")
             r.raise_for_status()
         except httpx.HTTPError as e:
             print(e)
             return None
-        return r.json()["heartrate"]
+
+        return r.json()["data"]["heart_rate"]
 
     def _validate_token(self):
         """Validate token against the API."""
         r = self.client.get("https://dev.pulsoid.net/api/v1/token/validate")
-        r.raise_for_status()
+
+        try:
+            r.raise_for_status()
+        except httpx.HTTPError as e:
+            if r.status_code == 403:
+                print("Pulsoid declined the token. Please check the ini file and update it.")
+            else:
+                print(e)
+            exit()
 
     def _get_client(self) -> httpx.Client:
         """Get a client with the token set in the header."""
@@ -39,19 +47,43 @@ class Pulsoid:
         )
 
 
+def process_ini_file():
+    """Make an empty config.ini file."""
+    global config
+
+    if config.read("config.ini"):
+        print("Config file exists.")
+        return
+
+    config["Login"] = {"API_TOKEN": "YOUR_API_TOKEN"}
+    config["Settings"] = {"HEART_RATE_FILE": "heartrate.txt"}
+    with open("config.ini", "w") as f:
+        config.write(f)
+
+    print("Config file created. Please edit it and run the program again.")
+    exit()
+
+
 def save_heart_rate(hr):
     """Save the heart rate to a file, catching exceptions since there might be race conditions."""
     try:
         with open(config["Settings"]["HEART_RATE_FILE"], "w") as f:
-            f.write(hr)
+            f.write(str(hr))
     except Exception as e:
         print(e)
 
 
-if __name__ == "__main__":
+def run():
+    """Run the program."""
+    process_ini_file()
     pulsoid = Pulsoid()
 
     while True:
+        time.sleep(0.5)
         if hr := pulsoid.get_heartrate():
             save_heart_rate(hr)
             print(hr)
+
+
+if __name__ == "__main__":
+    run()
